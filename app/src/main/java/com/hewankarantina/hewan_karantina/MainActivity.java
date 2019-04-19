06bb6a32ead2a1702efb166d961f9c61f6700317
Alvin <alvintandiardi@gmail.com>
2019-04-19 21:35:29 +0700

    Initial Commit

---
+++ app/src/main/java/com/hewankarantina/hewan_karantina/MainActivity.java
@@ -0,0 +1,26 @@
+package com.hewankarantina.hewan_karantina;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView daftar = (TextView)findViewById(R.id.textDaftar);
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+             Intent i = new Intent(MainActivity.this, DaftarActivity.class);
+             startActivity(i);
+            }
+        });
+    }
+}
